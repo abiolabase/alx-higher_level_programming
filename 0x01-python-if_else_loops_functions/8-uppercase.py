0ab76157640fcc2c14d0ffffb7011c403b5c9e03
Abiola Ajibowu <talk2sherifabiola@yahoo.com>
2024-01-04 01:17:39 -0800

    Fix indentation issue

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for c in str:
+        if ord(c)in range(97, 123):
+            num = 32
+        else:
+            num = 0
+        print("{:c}".format(ord(c) - num), end="")
+    print()
