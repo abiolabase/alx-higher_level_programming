3c224a18352bb0813d066bed2ff961948f3434f8
Abiola Ajibowu <talk2sherifabiola@yahoo.com>
2024-01-03 20:55:54 -0800

    My conditionals and loops

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+
+if number > 0:
+    ldigit = number % 10
+else:
+    ldigit = number % -10
+   
+if ldigit == 0:
+    state = "0"
+elif ldigit > 5:
+    state = "greater than 5"
+else:
+    state = "less than 6 and not 0"
+
+print(f"Last digit of {number} is {ldigit} and is {state}")
